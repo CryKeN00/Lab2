e748ce220319ab33ab05e24097086ae6ae1dcfd0
12 <12@DESKTOP-DBHHR8K>
2023-10-24 10:59:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Введите первую сторону треугольника");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону треугольника");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину основания треуголника");
+double c = Convert.ToDouble(Console.ReadLine());
+double p = (a + b + c) / 2;
+double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+if (a + b > c && a + c > b && c + b > a)
+{
+    Console.WriteLine($"Площадь треуголника будет = {S}");
+}
+else
+{
+    Console.WriteLine("Треугольник не существует");
+}
